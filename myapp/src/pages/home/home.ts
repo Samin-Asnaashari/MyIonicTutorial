da61228d7b7f73d107361119aa186861a6174a71
Samin.asnaashari <samin.asn73@gmail.com>
2019-01-26 17:22:02 +0100

    homepage

--- myapp/src/pages/home/home.ts
+++ myapp/src/pages/home/home.ts
@@ -6,6 +6,12 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
+  menuData = [
+    { title: 'Our Menu', pic:'../../assets/imgs/Menu/MenuD.png', pushPage: 'MenuPage' },
+    { title: 'Account', pic:'../../assets/imgs/Menu/Account.png', pushPage: 'AccountPage' },
+    { title: 'About Us', pic:'../../assets/imgs/Menu/About.png', pushPage: 'AboutPage' },
+    { title: 'Locations', pic:'../../assets/imgs/Menu/Locations.jpg', pushPage: 'LocationsPage' }
+  ];
 
   constructor(public navCtrl: NavController) {
 
